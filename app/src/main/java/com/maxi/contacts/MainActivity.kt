b79e24658ecb3fc852b626c1b3d45fb62c3042eb
maxi <arora.anubhav93@gmail.com>
2025-05-17 08:16:43 +0530

    Update the ui and call the parent composable

--- app/src/main/java/com/maxi/contacts/MainActivity.kt
+++ app/src/main/java/com/maxi/contacts/MainActivity.kt
@@ -1,25 +1,63 @@
 package com.maxi.contacts
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.maxi.contacts.ui.theme.ContactsTheme
+import androidx.compose.ui.res.stringResource
+import com.maxi.contacts.presentation.screens.ContactsScreen
+import com.maxi.contacts.presentation.ui.theme.ContactsTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ContactsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Log.d(TAG, "onCreate: $innerPadding")
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    topBar = {
+                        AppBar()
+                    }
+                ) { innerPadding ->
+                    App(
+                        Modifier
+                            .padding(innerPadding)
+                    )
                 }
             }
         }
     }
 }
 
-const val TAG = "MainActivityTAG"
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar() {
+    TopAppBar(
+        title = {
+            Text(text = stringResource(R.string.contacts))
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.onSurfaceVariant,
+            titleContentColor = MaterialTheme.colorScheme.surface
+        ),
+        scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    )
+}
+
+@Composable
+fun App(modifier: Modifier) {
+    ContactsScreen(modifier)
+}
