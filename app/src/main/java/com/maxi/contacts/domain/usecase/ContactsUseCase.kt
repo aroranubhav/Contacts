15679c6e454f4513f01c878ff8294de110266998
maxi <arora.anubhav93@gmail.com>
2025-05-19 20:13:36 +0530

    Update use case to emit list of contacts

--- app/src/main/java/com/maxi/contacts/domain/usecase/ContactsUseCase.kt
+++ app/src/main/java/com/maxi/contacts/domain/usecase/ContactsUseCase.kt
@@ -3,14 +3,16 @@ package com.maxi.contacts.domain.usecase
 import com.maxi.contacts.domain.model.Contact
 import com.maxi.contacts.domain.repository.ContactsRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class ContactsUseCase @Inject constructor(
     private val repository: ContactsRepository
 ) {
 
-    suspend operator fun invoke(): Flow<Map<String, List<Contact>>> {
+    suspend operator fun invoke(): Flow<List<Contact>> =
+        repository.getContacts()
+
+    /*suspend operator fun invoke(): Flow<Map<String, List<Contact>>> {
         var contacts = listOf<Contact>()
         repository.getContacts().collect {
             contacts = it
@@ -22,5 +24,5 @@ class ContactsUseCase @Inject constructor(
                 }
             )
         }
-    }
+    }*/
 }
